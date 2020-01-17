b8585b4f3654ce54114685de4ee1c4615467fe8d
Richard Hidalgo <hidalgo931@gmail.com>
2020-01-17 19:30:17 -0300

    feat: set env PUBLIC_PATH before import webpack config file

--- scripts/deploy-gh-pages.js
+++ scripts/deploy-gh-pages.js
@@ -1,9 +1,10 @@
 const ghpages = require('gh-pages');
 const webpack = require('webpack');
-const config = require('../webpack/production.config');
 
 const deploy = () => {
   console.log('building...');
+  process.env.PUBLIC_PATH = 'https://jokude.github.io/super-smash-bros-ui/';
+  const config = require('../webpack/production.config');
   webpack(config, (err, stats) => {
     if (err || stats.hasErrors()) {
       console.log(err);
