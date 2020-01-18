a84f62589840e52004115ed4d4992484023f01b3
Richard Hidalgo <hidalgo931@gmail.com>
2020-01-18 18:39:10 -0300

    fix: publish only after building success

--- scripts/deploy-gh-pages.js
+++ scripts/deploy-gh-pages.js
@@ -1,16 +1,20 @@
 const ghpages = require('gh-pages');
 const webpack = require('webpack');
 
-const deploy = () => {
+const build = onComplete => {
   console.log('building...');
   process.env.PUBLIC_PATH = 'https://jokude.github.io/super-smash-bros-ui/';
   const config = require('../webpack/production.config');
   webpack(config, (err, stats) => {
     if (err || stats.hasErrors()) {
       console.log(err);
+      return;
     }
+    onComplete();
   });
+};
 
+const publish = () => {
   console.log('deploying...');
   ghpages.publish('dist', err => {
     if (err) {
@@ -20,5 +24,7 @@ const deploy = () => {
     }
   });
 };
+
+const deploy = () => build(publish);
 
 deploy();
